e64c64dda77aaf1798b72135dfddf89ed249d627
ThiAABrown <antunesabrown@gmail.com>
2022-07-28 08:34:49 -0300

    Commitando o que foi feito até aqui

---
+++ igsSoftwareManager/core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from core.models import Employees, Departments
+
+admin.site.register(Employees)
+admin.site.register(Departments)
---
+++ igsSoftwareManager/core/models.py
@@ -0,0 +1,16 @@
+from os import name
+from django.db import models
+
+class Employees(models.Model):
+    name = models.CharField('Name', max_length=255)
+    email = models.CharField('E-mail', max_length=255)
+    # department = models.OneToOneField('Department', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name 
+
+class Departments(models.Model):
+    name = models.CharField('Name', max_length=255)
+
+    def __str__(self):
+        return self.name
