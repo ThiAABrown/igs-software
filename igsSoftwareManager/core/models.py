7dd57421a243f7ebb9ddc032b3738712b31d6afa
ThiAABrown <antunesabrown@gmail.com>
2022-07-28 20:49:47 -0300

    ADD validador de email

--- igsSoftwareManager/core/models.py
+++ igsSoftwareManager/core/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import validate_email
 
 class Employees(models.Model):
     name = models.CharField('Name', max_length=255)
-    email = models.CharField('E-mail', max_length=255)
+    email = models.CharField('E-mail', max_length=255, validators=[validate_email])
     department = models.OneToOneField('Departments', on_delete=models.CASCADE)
     
     def __str__(self):
